5f3d974cfb0c3da6af925af7ea9135e600904b80
Roope Turkki <roopeh.turkki@gmail.com>
2024-02-28 12:42:02 +0200

    client side functions + severs function stubs

--- assignment2/client.py
+++ assignment2/client.py
@@ -1,8 +1,51 @@
 from xmlrpc.client import ServerProxy
+from datetime import datetime
+
+PROXY_ADDRESS = "http://127.0.0.1:8000/RPC2"
+
+
+class Message:
+    """Class is used to represent the message that will be sent to the server"""
+
+    def __init__(self, topic: str, content: str) -> None:
+        self.topic: str = topic
+        self.content: str = content
+        self.timestamp: datetime = datetime.now().isoformat()
+
+
+def get_user_input() -> Message:
+    """Used to get the user input and create a Message object based on it"""
+    topic = input("Enter the topic of the message: ")
+    content = input("Enter the content of the message: ")
+    return Message(topic, content)
+
+
+def send_message(msg: Message) -> str:
+    """Used for sending a message to the server"""
+    try:
+        with ServerProxy(PROXY_ADDRESS) as proxy:
+            # Objects can be sent but only their __dict__ attribute is used, which is fine
+            proxy.add_new(msg)
+
+    except ConnectionRefusedError:
+        print("Could not connect to server.")
+
+
+def get_topic(topic: str) -> dict:
+    """Get a topic from server"""
+    try:
+        with ServerProxy(PROXY_ADDRESS) as proxy:
+            return proxy.get_topic(topic)
+
+    except ConnectionRefusedError:
+        print("Could not connect to server.")
+
 
 def main():
-    with ServerProxy("http://127.0.0.1:8000/RPC2") as proxy:
-        print(proxy.add_new("This is a test message"))
+    # get the user input and create a Message object based on it
+    msg = get_user_input()
+    send_message(msg)
+
 
 if __name__ == "__main__":
     main()
--- assignment2/server.py
+++ assignment2/server.py
@@ -1,20 +1,26 @@
 from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 
+
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ("/RPC2",)
+
 
 def main():
     with (SimpleXMLRPCServer(("127.0.0.1", 8000), requestHandler=RequestHandler)) as server:
         server.register_introspection_functions()
 
         @server.register_function(name="add_new")
-        def add_new(content: str):
+        def add_new(content):
             print(content)
             return "OK"
 
-       
+        @server.register_function(name="get_topic")
+        def get_topic(topic):
+            # TODO: implement
+            raise NotImplementedError()
+            return "No topic found."
+
         server.serve_forever()
-    
 
 
 if __name__ == "__main__":
