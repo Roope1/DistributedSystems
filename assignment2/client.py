e25ba69d6f06dc9cddbb278a7076894e38b09e9d
Roope Turkki <roopeh.turkki@gmail.com>
2024-03-10 13:58:09 +0200

    task done, added menu to client side + fixed adding notes on server

--- assignment2/client.py
+++ assignment2/client.py
@@ -7,17 +7,19 @@ PROXY_ADDRESS = "http://127.0.0.1:8000/RPC2"
 class Message:
     """Class is used to represent the message that will be sent to the server"""
 
-    def __init__(self, topic: str, content: str) -> None:
+    def __init__(self, topic: str, note: str, text: str) -> None:
         self.topic: str = topic
-        self.content: str = content
+        self.note: str = note
+        self.text: str = text
         self.timestamp: datetime = datetime.now().isoformat()
 
 
 def get_user_input() -> Message:
     """Used to get the user input and create a Message object based on it"""
     topic = input("Enter the topic of the message: ")
+    note = input("Enter a note for the message: ")
     content = input("Enter the content of the message: ")
-    return Message(topic, content)
+    return Message(topic, note, content)
 
 
 def send_message(msg: Message) -> str:
@@ -42,11 +44,28 @@ def get_topic(topic: str) -> str:
 
 
 def main():
-    # get the user input and create a Message object based on it
-    msg = get_user_input()
-    send_message(msg)
-    #topic = input("Enter the topic: ")
-    #print(get_topic(topic))
+    while True:
+        print("What do you want to do?")
+        print("1. Add a new note")
+        print("2. Get an existing topic")
+        print("3. Exit")
+        try:
+            choice = int(input())
+            match choice:
+                case 1:
+                    msg = get_user_input()
+                    send_message(msg)
+                case 2:
+                    topic = input("Enter the topic: ")
+                    print(get_topic(topic))
+                case 3:
+                    break
+                case _:
+                    print("Invalid input")
+                    continue
+        except ValueError:
+            print("Invalid input")
+            continue
 
 if __name__ == "__main__":
     main()
--- assignment2/server.py
+++ assignment2/server.py
@@ -7,38 +7,52 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ("/RPC2",)
 
 
+def create_new_note(topic, content):
+    new_note = ET.SubElement(topic, "note")
+    new_note.attrib["name"] = content["note"]
+    new_content = ET.SubElement(new_note, "text")
+    new_content.text = content["text"]
+    new_timestamp = ET.SubElement(new_note, "timestamp")
+    new_timestamp.text = content["timestamp"]
+    print("from function")
+    ET.dump(new_note)
+    return new_note
+
+def create_new_topic(content):
+    new_topic = ET.Element("topic")
+    new_topic.attrib["name"] = content["topic"]
+    new_note = create_new_note(new_topic, content)
+
+    return new_topic
+
 def main():
     with (SimpleXMLRPCServer(("127.0.0.1", PORT), requestHandler=RequestHandler)) as server:
         server.register_introspection_functions()
 
         @server.register_function(name="add_new")
         def add_new(content):
-            print(content)
             # Open the "database" xml file
             with (open("db.xml", "r")) as f:
                 # read the existing "db"
                 root = ET.fromstring(f.read())
             # try find the topic
-            print("got here")
             res = root.findall(f"./topic[@name='{content['topic']}']")
             # if topic exists append to it 
             if (len(res) > 0):
-                # TODO: append new item
-                pass 
+                create_new_note(res[0], content)
+                with (open("db.xml", "w")) as f:
+                    f.write(ET.tostring(root, encoding="unicode"))
+                    print("file written")
+                return "OK"
             # else create a new topic
             else: 
-                new_topic = ET.Element("topic")
-                new_topic.attrib["name"] = content["topic"]
-                new_content = ET.SubElement(new_topic, "content")
-                new_content.text = content["content"]
-                root.append(new_topic)
+                root.append(create_new_topic(content))
                 print("new topic created")
                 # write to file
                 with (open("db.xml", "w")) as f:
                     f.write(ET.tostring(root, encoding="unicode"))
                     print("file written")
-                    
-            return "OK"
+                return "OK"
 
         @server.register_function(name="get_topic")
         def get_topic(topic):
